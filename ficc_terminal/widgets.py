154216be78202cfb96a3af6484787f6e52c75960
Antoine-Sorba <sorbaantoine02@gmail.com>
2026-08-11 16:04:14 +0100

    Replace embedded widgets with direct chart links

--- ficc_terminal/widgets.py
+++ ficc_terminal/widgets.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-import json
+from urllib.parse import quote
 
 
 ESSENTIAL_MARKETS = {
@@ -56,120 +56,7 @@ ESSENTIAL_MARKETS = {
 }
 
 
-def tradingview_ticker_html() -> str:
-    config = {
-        "symbols": [
-            {"description": "US 10Y yield", "proName": "TVC:US10Y"},
-            {"description": "Dollar index", "proName": "TVC:DXY"},
-            {"description": "EUR/USD", "proName": "FX:EURUSD"},
-            {"description": "USD/JPY", "proName": "FX:USDJPY"},
-            {"description": "Brent", "proName": "TVC:UKOIL"},
-            {"description": "Gold", "proName": "COMEX:GC1!"},
-            {"description": "S&P futures", "proName": "CME_MINI:ES1!"},
-        ],
-        "showSymbolLogo": True,
-        "isTransparent": True,
-        "displayMode": "adaptive",
-        "colorTheme": "light",
-        "locale": "en",
-    }
-    return f"""
-    <div class="tradingview-widget-container">
-      <div class="tradingview-widget-container__widget"></div>
-      <div class="tradingview-widget-copyright">
-        <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">Market data by TradingView</a>
-      </div>
-      <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
-      {json.dumps(config)}
-      </script>
-    </div>
-    """
+def tradingview_chart_url(symbol: str) -> str:
+    """Return a direct TradingView chart link without embedding third-party code."""
 
-
-def tradingview_overview_html() -> str:
-    config = {
-        "colorTheme": "light",
-        "dateRange": "1D",
-        "showChart": True,
-        "locale": "en",
-        "largeChartUrl": "",
-        "isTransparent": True,
-        "showSymbolLogo": True,
-        "showFloatingTooltip": True,
-        "width": "100%",
-        "height": 610,
-        "plotLineColorGrowing": "rgba(18, 64, 50, 1)",
-        "plotLineColorFalling": "rgba(190, 79, 55, 1)",
-        "tabs": [
-            {
-                "title": "Rates & FX",
-                "symbols": [
-                    {"s": "CBOT:ZN1!", "d": "US 10Y future"},
-                    {"s": "EUREX:FGBL1!", "d": "Bund future"},
-                    {"s": "FX:EURUSD", "d": "EUR/USD"},
-                    {"s": "FX:GBPUSD", "d": "GBP/USD"},
-                    {"s": "FX:USDJPY", "d": "USD/JPY"},
-                ],
-            },
-            {
-                "title": "Commodities",
-                "symbols": [
-                    {"s": "NYMEX:CL1!", "d": "WTI"},
-                    {"s": "TVC:UKOIL", "d": "Brent"},
-                    {"s": "COMEX:GC1!", "d": "Gold"},
-                    {"s": "COMEX:HG1!", "d": "Copper"},
-                ],
-            },
-            {
-                "title": "Equity context",
-                "symbols": [
-                    {"s": "FOREXCOM:SPXUSD", "d": "S&P 500"},
-                    {"s": "NASDAQ:NDX", "d": "Nasdaq 100"},
-                    {"s": "TVC:UKX", "d": "FTSE 100"},
-                    {"s": "TVC:NI225", "d": "Nikkei 225"},
-                ],
-            },
-        ],
-    }
-    return f"""
-    <div class="tradingview-widget-container">
-      <div class="tradingview-widget-container__widget"></div>
-      <div class="tradingview-widget-copyright">
-        <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">Provider-hosted market overview by TradingView</a>
-      </div>
-      <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js" async>
-      {json.dumps(config)}
-      </script>
-    </div>
-    """
-
-
-def tradingview_advanced_chart_html(asset_class: str) -> str:
-    market = ESSENTIAL_MARKETS[asset_class]
-    config = {
-        "autosize": True,
-        "symbol": market["default"],
-        "interval": "60",
-        "timezone": "Europe/London",
-        "theme": "light",
-        "backgroundColor": "rgba(255, 254, 249, 1)",
-        "style": "1",
-        "withdateranges": True,
-        "hide_side_toolbar": True,
-        "allow_symbol_change": True,
-        "save_image": False,
-        "locale": "en",
-        "watchlist": market["watchlist"],
-        "support_host": "https://www.tradingview.com",
-    }
-    return f"""
-    <div class="tradingview-widget-container" style="height:100%;width:100%">
-      <div class="tradingview-widget-container__widget" style="height:calc(100% - 28px);width:100%"></div>
-      <div class="tradingview-widget-copyright">
-        <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">Interactive chart by TradingView</a>
-      </div>
-      <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js" async>
-      {json.dumps(config)}
-      </script>
-    </div>
-    """
+    return f"https://www.tradingview.com/chart/?symbol={quote(symbol, safe='')}"
